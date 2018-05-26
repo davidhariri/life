bf1cfe68bc3b559371389f819a85217521187fcc
David Hariri <david.hariri@gmail.com>
2018-05-26 17:18:29 -0400

    Finish adding styles from dhariri.com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import API from './models/API';
 import Note from './views/Note';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
--- src/views/Note.js
+++ src/views/Note.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Note.css';
 
 class Note extends Component {
   createMarkup() {
