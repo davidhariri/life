1c6c4891823e2b751af4770bbf46f1ee29a67a50
David Hariri <david.hariri@gmail.com>
2018-10-05 17:42:00 -0400

    Adds date formatting

--- src/views/Note.js
+++ src/views/Note.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import moment from 'moment';
 import './Note.css';
 
 class Note extends Component {
+  renderCreatedDate(ISODate) {
+    const _date = moment(this.props.created);
+    const humanDateFormat = _date.format('MM/DD/YYYY');
+
+    return <time dateTime={ISODate}>{humanDateFormat}</time>;
+  }
+
   render() {
     return (
       <div className="Note">
         <div className="Note__content">{this.props.text}</div>
         <div className="Note__info">
-          <time dateTime={`${this.props.created}`}>{this.props.created}</time> —{' '}
+          {this.renderCreatedDate()} —{' '}
           <a
             href={`http://maps.apple.com/?ll=${this.props.location[0]},${
               this.props.location[1]
