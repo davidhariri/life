81f485cd8a14bdc08e5696076498b335235342b9
David Hariri <david.hariri@gmail.com>
2019-01-14 22:03:53 -0500

    New Media objects with color being passed to Views (#35)

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import API from '../models/API';
 import Post from '../views/Post';
 import Nav from '../views/Nav';
+import MetaTags from 'react-meta-tags';
+import cutOffString from '../helpers/cutOffString';
 
 class Posts extends Component {
   constructor(props) {
@@ -21,9 +23,37 @@ class Posts extends Component {
     });
   }
 
+  getTitle() {
+    const c = this.state.post.comment;
+    return c ? cutOffString(c, 24) : 'A post';
+  }
+
+  getMedia() {
+    if (this.state.post !== undefined && this.state.post.media.length) {
+      return this.state.post.media[0].url_optimized;
+    }
+  }
+
+  getMetaTags() {
+    if (this.state.post === undefined) {
+      return undefined;
+    }
+
+    return (
+      <MetaTags>
+        <title>{this.getTitle()}</title>
+        <meta property="og:title" content={this.getTitle()} />
+        <meta name="description" content={this.state.post.comment} />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:image" content={this.getMedia()} />
+      </MetaTags>
+    );
+  }
+
   render() {
     return (
       <div className="Posts">
+        {this.getMetaTags()}
         <Nav />
         {this.state.post ? (
           <Post {...this.state.post} show_tweet={true} />
